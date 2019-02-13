7024fe6a11e30bc92dbd625c39fa614424db860e
tree <sunshukang30@163.com>
2019-02-13 11:24:10 +0800

    todo view: add view_func

--- app/v1000/views/todo.py
+++ app/v1000/views/todo.py
@@ -1,8 +1,9 @@
 from flask import request
 from ..views import api
 from app.utils import response_succ, CommonError, get_unix_time_tuple, login_require
-from app.utils.ext import g
+from app.utils.ext import g, NoResultFound
 from app.models import db, TodoModel
+
 
 @api.route("/todo/add", methods=["POST"])
 @login_require
@@ -22,21 +23,32 @@ def add_todo():
     return response_succ(body=result)
 
 
+def set_todo_state(todo_id, state):
+    result = None
+    try:
+        todo = db.session.query(TodoModel).filter_by(todo_id=todo_id).one()
+        if not todo:
+            return result
+        todo.todo_state = state
+        db.session.commit()
+        result = {
+            "todo_id": todo.todo_id,
+            "todo_title": todo.todo_title,
+            "todo_state": todo.todo_state
+        }
+    except NoResultFound as e:
+        result = None
+    return result
+
+
 @api.route("todo/finish", methods=["POST"])
 @login_require
 def finish_todo():
     params = request.values
     todo_id = params.get("todo_id")
-    todo = db.session.query(TodoModel).filter_by(todo_id=todo_id).one()
-    if not todo:
-        return CommonError.get_error(44000)
-    todo.todo_state = 2
-    db.session.commit()
-    result = {
-        "todo_id": todo.todo_id,
-        "todo_title": todo.todo_title,
-        "todo_state": todo.todo_state
-    }
+    result = set_todo_state(todo_id, 2)
+    if not result:
+        return CommonError.get_error(40000)
     return response_succ(body=result)
 
 
@@ -45,14 +57,47 @@ def finish_todo():
 def remove_todo():
     params = request.values
     todo_id = params.get("todo_id")
-    todo = db.session.query(TodoModel).filter_by(todo_id=todo_id).one()
-    if not todo:
-        return CommonError.get_error(44000)
-    todo.todo_state = 2
-    db.session.commit()
-    result = {
-        "todo_id": todo.todo_id,
-        "todo_title": todo.todo_title,
-        "todo_state": todo.todo_state
-    }
+    result = set_todo_state(todo_id, 3)
+    if not result:
+        return CommonError.get_error(40000)
+    return response_succ(body=result)
+
+
+@api.route("todo/filter/<string:filter>", methods=["POST"])
+@login_require
+def filter_todo(filter: str=None):
+    params = request.values
+    option_filter = filter or "all"
+    todos = db.session.query(TodoModel).filter(TodoModel.bind_user_id==g.current_user.id)
+    
+    if option_filter == "undo":
+        todos = todos.filter(TodoModel.todo_state == 1).all()
+    if option_filter == "done":
+        todos = todos.filter(TodoModel.todo_state == 2).all()
+    if option_filter == "all":
+        todos = todos.filter(TodoModel.todo_state!=3).all()
+    if not todos:
+        return CommonError.get_error(40000)
+
+    if len(todos) == 0:
+        return CommonError.error_toast(msg="没有待办")
+    
+    result = []
+    for todo in todos:
+        result.append({
+            "todo_id": todo.todo_id,
+            "todo_title": todo.todo_title,
+            "todo_state": todo.todo_state
+        })
+    return response_succ(body=result)
+
+
+@api.route("todo/undo", methods=["POST"])
+@login_require
+def undo_todo():
+    params = request.values
+    todo_id = params.get("todo_id")
+    result = set_todo_state(todo_id, 1)
+    if not result:
+        return CommonError.get_error(40000)
     return response_succ(body=result)
