a4bb14a853b6e1af4a4c995a544cdb521089e840
tree <sunshukang30@163.com>
2019-03-01 17:52:18 +0800

    添加了多应用支持

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 from app import create_app
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
 from app.utils.ext import render_template, request
 from app.utils.response_factory import response_succ
 from app.utils import login_require
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+main_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+application = DispatcherMiddleware(main_app, {
+})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8091, debug=True)
+    run_simple("0.0.0.0", port=8091, application=application, use_reloader=True, use_debugger=True)
