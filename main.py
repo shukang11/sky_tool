aa37e8c0d4f3a326eb2fd65c26a4b33a434abf72
tree <sunshukang30@163.com>
2019-03-01 17:38:37 +0800

    移除了测试的route

--- main.py
+++ main.py
@@ -6,16 +6,5 @@ from app.utils import login_require
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-@app.route('/<string:file>', methods=['GET'])
-def routeMap(file: str):
-    return render_template(file)
-
-@app.route('/test', methods=['GET', 'POST'])
-def on_test():
-    params = request.values
-    return response_succ(body=params or {"r": "on test"})
-
 if __name__ == '__main__':
-    from werkzeug.contrib.fixers import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(host='0.0.0.0', port=8091, debug=True)
