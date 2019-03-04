26ec0aade37414712279c6dd1491ea5dbad07ea8
tree <sunshukang30@163.com>
2019-03-04 11:02:40 +0800

    update gitignore

--- app/v1000/__init__.py
+++ app/v1000/__init__.py
@@ -3,12 +3,7 @@ import app
 
 api = Blueprint('api1_0', __name__)
 
-from .views import index
-from .views import upload
-from .views import user
-from .views import permission
-from .views import tool_view
-from .views import todo
+from .views import *
 
 # 注册蓝图
 app.fetch_route(api, '/api/v1000')
