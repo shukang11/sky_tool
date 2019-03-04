0044ba420f63393be3b7b75c1d6acb9459b94791
tree <sunshukang30@163.com>
2019-03-04 15:32:33 +0800

    添加了数据流的视图

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,26 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, Response
+from flask_socketio import send, emit
+from chat import socketio
 
 page = Blueprint("page", __name__)
 
-@page.route("/")
-def index():
-    return render_template("index.html")
+@socketio.on("client_event")
+def handle_client_event(json):
+    emit("server_response", {'data': str(json)})
+
+@page.route("/<string:file>")
+def render_file(file="index.html"):
+    return render_template(file)
+
+
+@page.route("/large.csv")
+def generate_large_csv():
+    """
+    下面是一个简单的视图函数，这一视图函数实时生成大量的 CSV 数据， 
+    这一技巧使用了一个内部函数，这一函数使用生成器来生成数据，
+    并且稍后激发这个生成器函数时，把返回值传递给一个 response 对象
+    """
+    def generate():
+        for row in range(0, 10000):
+            yield ','.join(str(row)) + '\n'
+    return Response(generate(), mimetype='text/csv')
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
 
 from app import create_app as create_main_app
-from chat import create_app as create_chat_app
+from chat import create_app as create_chat_app, socketio
 from app.utils.ext import render_template, request
 from app.utils.response_factory import response_succ
 from app.utils import login_require
@@ -16,6 +16,6 @@ application = DispatcherMiddleware(main_app, {
 })
 
 if __name__ == '__main__':
-    run_simple("0.0.0.0", port=8091, application=application, use_reloader=True, use_debugger=True)
-    # from chat import socketio
-    # socketio.run(chat_app, port=8091)
+    # run_simple("0.0.0.0", port=8091, application=application, use_reloader=True, use_debugger=True)
+    from chat import socketio
+    socketio.run(chat_app, port=8091)
