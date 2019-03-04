a750d639399f045b8a518fad6f33bbebd8e27596
tree <sunshukang30@163.com>
2019-03-04 11:24:16 +0800

    解决了views 导入失败的问题

--- app/v1000/__init__.py
+++ app/v1000/__init__.py
@@ -3,7 +3,12 @@ import app
 
 api = Blueprint('api1_0', __name__)
 
-from .views import *
+from .views import index
+from .views import permission
+from .views import todo
+from .views import tool_view
+from .views import upload
+from .views import user
 
 # 注册蓝图
 app.fetch_route(api, '/api/v1000')
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from app.utils.response_factory import response_succ
 from app.utils import login_require
 
 
-main_app = create_main_app(os.getenv('FLASK_CONFIG') or 'default')
+main_app = create_main_app('default')
 
 application = DispatcherMiddleware(main_app, {
 })
 
 if __name__ == '__main__':
+    # main_app.run("0.0.0.0", port=8091, debug=True)
     run_simple("0.0.0.0", port=8091, application=application, use_reloader=True, use_debugger=True)
