85cbb88521a1d2a4c716119a1ca89801f6eb1bb6
tree <sunshukang30@163.com>
2019-03-18 10:49:41 +0800

    避免重复插入rss_content

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -22,23 +22,6 @@ class CallBackTask(Task):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         return super(CallBackTask, self).on_failure(exc, task_id, args, kwargs, einfo)
 
-@celery.task(bind=True, ignore_result=True, default_retry_delay=300, max_retries=3)
-def async_email_to(subject: str, body: str, recipients: list):
-    user = "sunshukang30@163.com"
-    password = "a12345678"  # 授权码
-    receivers = recipients
-    sender = user
-    mail = Mail("smtp.163.com", user, password, 465,  True, sender, 10)
-    message = Message(subject=subject or "",
-                        recipients=receivers, body=body or "", sender=sender)
-    mail.send(message)
-
-@celery.task(default_retry_delay=300, max_retries=3, ignore_result=True)
-def async_parser_feed(url: str):
-    result = parser_feed(url)
-    parse_inner(url, result)
-    return result
-
 
 @celery.task(base=CallBackTask)  # 指定回调
 def add(x: int, y: int, *args, **kwargs):
@@ -53,4 +36,36 @@ def mul(self, x: int, y: int, *args, **kwargs):
         self.request))
     time.sleep(5)
     result = x + y
+    return result
+
+
+@celery.task(bind=True, ignore_result=True, default_retry_delay=300, max_retries=3)
+def async_email_to(subject: str, body: str, recipients: list):
+    """
+    send email
+    Args:
+    subject: 邮件主题
+    body: 邮件内容， 当前只支持字符
+    recipients: 收件人
+    Return: None
+    """
+    user = "sunshukang30@163.com"
+    password = "a12345678"  # 授权码
+    receivers = recipients
+    sender = user
+    mail = Mail("smtp.163.com", user, password, 465,  True, sender, 10)
+    message = Message(subject=subject or "",
+                        recipients=receivers, body=body or "", sender=sender)
+    mail.send(message)
+
+@celery.task(default_retry_delay=300, max_retries=3, ignore_result=True)
+def async_parser_feed(url: str):
+    """
+    开始解析rss任务
+    Args:
+    url: rss地址
+    Return: 字典，包含了解析的结果
+    """
+    result = parser_feed(url)
+    parse_inner(url, result)
     return result
