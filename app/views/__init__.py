fb14b9b63d2c889ba17e043c941aaee5c6046d5d
tree <sunshukang30@163.com>
2019-03-24 21:36:06 +0800

    .

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -11,7 +11,7 @@ from app.views import tool_view
 from app.views import upload
 from app.views import user
 from app.views import rss
-from app.views import rooot_bp
+from app.views import root_bp
 
 # 注册蓝图
 app.fetch_route(api, '/api')
