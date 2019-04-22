0a1575fd3ad2f83f17091a3e3ed4655a97460630
jiazifa <2332532718@qq.com>
2019-04-22 14:39:24 +0800

    对 rss 列表接口重写，使用原生的语句

--- app/views/rss.py
+++ app/views/rss.py
@@ -61,17 +61,43 @@ def add_rss_source():
 @api.route("/rss/limit", methods=["POST", "GET"])
 @login_require
 def list_rss():
+    # params = request.values or request.get_json() or {}
+    # all_rss = db.session.query(RssModel).filter(RssModel.rss_id == RssUserModel.rss_id).filter(RssUserModel.user_id == g.current_user.id).all()
+    # result = []
+    # if not all_rss:
+    #     return CommonError.error_toast(msg="没有订阅")
+    # for rss in all_rss:
+    #     result.append({
+    #         "rss_id": rss.rss_id,
+    #         "rss_link": rss.rss_link,
+    #     })
+    """ rss_base list, query from bao_rss """
     params = request.values or request.get_json() or {}
-    all_rss = db.session.query(RssModel).filter(RssModel.rss_id == RssUserModel.rss_id).filter(RssUserModel.user_id == g.current_user.id).all()
-    result = []
-    if not all_rss:
-        return CommonError.error_toast(msg="没有订阅")
-    for rss in all_rss:
-        result.append({
-            "rss_id": rss.rss_id,
-            "rss_link": rss.rss_link,
-        })
-    return response_succ(body=result)
+    bind_user_id = g.current_user.id
+    # pages 
+    pages = params.get('pages') or 0
+    limit = params.get('limit') or 10
+    sql = ''
+    sql += """
+    SELECT bao_rss.rss_id, bao_rss.rss_link, bao_rss.add_time FROM bao_rss
+    WHERE bao_rss.rss_id IN (
+    SELECT rss_id FROM bao_rss_user WHERE bao_rss_user.user_id={user_id}
+    )
+    ORDER BY add_time DESC LIMIT {limit} OFFSET {offset};
+    """.format(user_id=bind_user_id, limit=limit, offset=pages*limit)
+    # sqlalchemy执行sql
+    data_query = db.session.execute(sql)
+    total = data_query.rowcount
+    payload = {}
+    payload['total'] = total
+    payload['pages'] = pages
+    payload['limit'] = limit
+    payload['list'] = [{
+        'link': item['rss_link'],
+        'add_time': item['add_time'],
+        'id': item['rss_id'],
+    } for item in data_query.fetchall()]
+    return response_succ(body=payload)
 
 
 @api.route("/rss/parse", methods=["POST", "GET"])
@@ -100,16 +126,26 @@ def task_parser_backend():
     return response_succ(body=payload)
 
 
+# 获得 bao_rss_content 列表的接口(将废弃)
 @api.route('/rss/content/list', methods=['POST'])
 @login_require
 def rss_content_list():
     params = request.values or request.get_json() or {}
-    pages = params.get('pages') or 0
-    limit = params.get('limit') or 10
-    time_desc = params.get('time_is_desc') or 0 # 0 升序 1 降序
+    pages = int(params.get('pages') or 0)
+    limit = int(params.get('limit') or 10)
+    time_desc = bool(params.get('time_is_desc') or 0) # 0 升序 1 降序
+    bind_user_id = g.current_user.id
     sql = """
-    SELECT * FROM bao_rss_content order by add_time {} limit {} offset {} ;
-    """.format('desc' if time_desc else 'asc', limit, pages*limit)
+    SELECT * FROM bao_rss_content
+    WHERE bao_rss_content.content_base IN (
+        SELECT bao_rss.rss_link
+        FROM bao_rss
+        WHERE bao_rss.rss_id IN (
+            SELECT rss_id FROM bao_rss_user WHERE bao_rss_user.user_id={user_id}
+        )
+    )
+    ORDER BY add_time {order} limit {limit} offset {offset};
+    """.format(order='DESC' if time_desc else 'ASC', limit=limit, offset=pages*limit, user_id=bind_user_id)
     # sqlalchemy执行sql
     data_query = db.session.execute(sql)
     total = data_query.rowcount
@@ -125,3 +161,37 @@ def rss_content_list():
         'id': item['content_id'],
     } for item in data_query.fetchall()]
     return response_succ(body=payload)
+
+
+@api.route('/rss/content/filter', methods=['POST'])
+@login_require
+def rss_content_filter():
+    """ 
+    filter rss content
+    Args:
+    bases: list, rss_ids
+    pages: -
+    limit: -
+    tags: not yet
+    key_words: not yet
+    """
+    params = request.values or request.get_json() or {}
+    bind_user_id = g.current_user.id
+    # 添加根据 base 的 filter
+    base = params.get('bases') or []
+    # pages 
+    pages = params.get('pages') or 0
+    limit = params.get('limit') or 10
+
+    # tag 
+    tags = params.get('tags') or []
+
+    # key words
+    key_words = params.get('key_word') or ''
+
+    sql = ''
+    sql += 'SELECT * '
+    sql += 'FROM bao_rss_content, bao_rss, bao_rss_user '
+    sql += 'ORDER BY add_time {} LIMIT {} OFFSET {} ;'.format('desc' if time_desc else 'asc', limit, pages*limit)
+
+    pass
