c3b1a927cccdf6ef34f0d3bbf4b1992d4042ef15
jiazifa <2332532718@qq.com>
2019-04-24 09:52:04 +0800

    remove test_rss

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -1,30 +1,30 @@
-from tests import TestBase
+# from tests import TestBase
 
-class TestRss(TestBase):
+# class TestRss(TestBase):
 
-    def test_add_rss(self):
-        rss_link = "https://www.zhihu.com/rss"
-        token = self.login()
-        params = {
-            "source": rss_link,
-            "token": token
-        }
-        rv = self._client.post("/api/rss/add", json=params)
-        assert rv.status_code == 200
-        assert rv.json["rss_id"] != None
+#     def test_add_rss(self):
+#         rss_link = "https://www.zhihu.com/rss"
+#         token = self.login()
+#         params = {
+#             "source": rss_link,
+#             "token": token
+#         }
+#         rv = self._client.post("/api/rss/add", json=params)
+#         assert rv.status_code == 200
+#         assert rv.json["rss_id"] != None
 
-    def test_list_rss(self):
-        token = self.login()
-        params = {
-            "token": token
-        }
-        rv = self._client.post("/api/rss/limit", json=params)
-        assert rv.status_code == 200
+#     def test_list_rss(self):
+#         token = self.login()
+#         params = {
+#             "token": token
+#         }
+#         rv = self._client.post("/api/rss/limit", json=params)
+#         assert rv.status_code == 200
 
-    def test_list_rss_content(self):
-        token = self.login()
-        params = {
-            "token": token
-        }
-        rv = self._client.post("/api/rss/content/list", json=params)
-        assert rv.status_code == 200
+#     def test_list_rss_content(self):
+#         token = self.login()
+#         params = {
+#             "token": token
+#         }
+#         rv = self._client.post("/api/rss/content/list", json=params)
+#         assert rv.status_code == 200
