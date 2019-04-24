dc8bc4c60b44c81e2a70d7cd8c63d3dc9f2b7e11
jiazifa <2332532718@qq.com>
2019-04-24 10:11:38 +0800

    测试

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -1,17 +1,17 @@
-# from tests import TestBase
+from tests import TestBase
 
-# class TestRss(TestBase):
+class TestRss(TestBase):
 
-#     def test_add_rss(self):
-#         rss_link = "https://www.zhihu.com/rss"
-#         token = self.login()
-#         params = {
-#             "source": rss_link,
-#             "token": token
-#         }
-#         rv = self._client.post("/api/rss/add", json=params)
-#         assert rv.status_code == 200
-#         assert rv.json["rss_id"] != None
+    def test_add_rss(self):
+        rss_link = "https://www.zhihu.com/rss"
+        token = self.login()
+        params = {
+            "source": rss_link,
+            "token": token
+        }
+        rv = self._client.post("/api/rss/add", json=params)
+        assert rv.status_code == 200
+        assert rv.json["rss_id"] != None
 
 #     def test_list_rss(self):
 #         token = self.login()
